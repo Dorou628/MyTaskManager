af087727df0407c8faa86c0e937606d5038413c6
DOROU <fuerwen00@gmail.com>
2025-05-06 23:18:04 +0800

    语音更改V1

--- Speech.cs
+++ Speech.cs
@@ -25,48 +25,36 @@ namespace MyTaskManager
             //InitRecognizer();
         }
 
-        private void StartSpeech_Click(object sender, EventArgs e)
+        private async void StartSpeech_Click(object sender, EventArgs e)
         {
             if (!isRecognizing)
             {
                 txtSpeechContent.Clear();
-                speechHelper = new SpeechHelper();
-                waveIn = new WaveInEvent();
-                waveIn.WaveFormat = new WaveFormat(16000, 16, 1);
 
-                var writer = new WaveFileWriter(audioFilePath, waveIn.WaveFormat);
-                waveIn.DataAvailable += (s, a) => writer.Write(a.Buffer, 0, a.BytesRecorded);
-                waveIn.RecordingStopped += async (s, a) =>
-                {
-                    writer.Dispose();
-                    waveIn.Dispose();
-
-                    // 获取 token
-                    string apiKey = "4U0drKRhd1JdywJ1wut41tU0";
-                    string secretKey = "E3yp9yuBd3S7jy4naSrubbakT0Z7C3HC";
-                    string accessToken = await speechHelper.GetAccessTokenAsync(apiKey, secretKey);
-
-                    // 调用识别
-                    try
-                    {
-                        string result = await speechHelper.RecognizeSpeechAsync(audioFilePath, accessToken);
-                        txtSpeechContent.Text = result;
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show("识别失败：" + ex.Message);
-                    }
-                };
-
-                waveIn.StartRecording();
+                speechHelper.RecordAudio(audioFilePath); // 开始录音
                 StartSpeech.Text = "停止识别";
                 isRecognizing = true;
             }
             else
             {
-                waveIn.StopRecording();
+                speechHelper.StopRecording(); // 停止录音
                 StartSpeech.Text = "开始识别";
                 isRecognizing = false;
+
+                // 获取 token
+                string apiKey = "4U0drKRhd1JdywJ1wut41tU0";
+                string secretKey = "E3yp9yuBd3S7jy4naSrubbakT0Z7C3HC";
+                string accessToken = await speechHelper.GetAccessTokenAsync(apiKey, secretKey);
+
+                try
+                {
+                    string result = await speechHelper.RecognizeSpeechAsync(audioFilePath, accessToken);
+                    txtSpeechContent.Text = result;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("识别失败：" + ex.Message);
+                }
             }
         }
     }
